170eeb3524c3c03d43d2ad1ebd1f32379811770c
Anthony Martinez <Martinezanthony213@gmail.com>
2019-07-19 14:50:32 -0700

    testing CI functionality

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.91.238.155',
+      host: '3.81.10.121',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
